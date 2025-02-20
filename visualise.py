1148aabdbeaf89347c4aaca29146054e3b8d7f56
Dipesh2616Prabhakar <138284206+Dipesh2616Prabhakar@users.noreply.github.com>
2025-02-20 19:16:10 +0530

    visualise.py
    This is the half code of python.

---
+++ visualise.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# Read the results file
+with open("results.txt", "r") as file:
+    data = file.readlines()
+
+# Process data
+algorithms = []
+faults = []
+for line in data:
+    algo, fault_count = line.split()
+    algorithms.append(algo)
+    faults.append(int(fault_count))
+
+# Create a bar chart
+fig, ax = plt.subplots()
+bars = ax.bar(algorithms, faults, color=['orange', 'violet', 'green'])
+
+# Labeling the chart
+plt.xlabel("Page Replacement Algorithms")
+plt.ylabel("Total Page Faults")
+plt.title("Comparison of Page Replacement Algorithms")
